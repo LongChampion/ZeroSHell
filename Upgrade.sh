b0f53579427a322bb83a136fe95446d0941f2c42
LongChampion <ThePalazin@Gmail.com>
2020-06-09 22:58:21 +0700

    Upgrade.sh no longer install MesloNerdFont

--- Upgrade.sh
+++ Upgrade.sh
@@ -23,14 +23,6 @@ else
 fi
 echo
 
-echo -e "\e[1;33m[*] \e[0;32mInstalling Meslo-Nerd-Font-Complete font\e[0m"
-wget https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/L-DZ/Regular/complete/Meslo%20LG%20L%20DZ%20Regular%20Nerd%20Font%20Complete.ttf
-wget https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/L-DZ/Bold/complete/Meslo%20LG%20L%20DZ%20Bold%20Nerd%20Font%20Complete.ttf
-wget https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/L-DZ/Italic/complete/Meslo%20LG%20L%20DZ%20Italic%20Nerd%20Font%20Complete.ttf
-wget https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/L-DZ/Bold-Italic/complete/Meslo%20LG%20L%20DZ%20Bold%20Italic%20Nerd%20Font%20Complete.ttf
-sudo mv ./*.ttf /usr/share/fonts/TTF/
-echo
-
 echo -e "\e[1;33m[*] \e[0;32mUpdating all sub-repository ...\e[0m"
 git submodule update --remote --recursive --init --depth 1
 echo
